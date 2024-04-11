e7d206e2c3def798f0a54f9445eca695e34672b7
Fran Dios <fran.dios@shopify.com>
2024-04-11 15:15:28 +0200

    Update to next version

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
-import {hydrogen, oxygen} from '@shopify/cli-hydrogen/experimental-vite';
+import {hydrogen} from '@shopify/hydrogen/vite';
+import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -7,7 +8,7 @@ export default defineConfig({
   plugins: [
     hydrogen(),
     oxygen(),
-    remix({buildDirectory: 'dist'}),
+    remix({presets: [hydrogen.preset()]}),
     tsconfigPaths(),
   ],
   ssr: {
