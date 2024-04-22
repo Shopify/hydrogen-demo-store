32c6dcb77852e91ce26d5f41e37ae9063d5b6e83
Bret Little <bret.little@shopify.com>
2024-04-22 14:09:04 -0400

    Add perf test

---
+++ app/routes/resource.tsx
@@ -0,0 +1,11 @@
+import {json, type LoaderFunctionArgs} from '@remix-run/server-runtime';
+
+export async function loader({context}: LoaderFunctionArgs) {
+  return json({
+    result: await context.storefront.query(`query getShop {
+        shop {
+          name
+        }
+      }`),
+  });
+}
---
+++ app/routes/test.tsx
@@ -0,0 +1,63 @@
+import {useEffect, useRef, useState} from 'react';
+import {createStorefrontApiClient} from '@shopify/storefront-api-client';
+import type {LoaderFunctionArgs} from '@remix-run/server-runtime';
+import {useLoaderData} from '@remix-run/react';
+
+export async function loader({context}: LoaderFunctionArgs) {
+  return {
+    storeDomain: context.env.PUBLIC_STORE_DOMAIN,
+    publicAccessToken: context.env.PUBLIC_STOREFRONT_API_TOKEN,
+  };
+}
+
+export default function MakeRequests() {
+  const [direct, setDirect] = useState({time: null});
+  const [oxygen, setOxygen] = useState({time: null});
+  const requestMade = useRef(false);
+
+  const data = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (!requestMade.current) {
+      performance.measure('oxygen');
+      fetch('/resource')
+        .then((resp) => resp.json())
+        .then(() => {
+          setOxygen(performance.measure('oxygen'));
+        });
+
+      const client = createStorefrontApiClient({
+        storeDomain: data.storeDomain,
+        apiVersion: '2023-10',
+        publicAccessToken: data.publicAccessToken,
+      });
+
+      performance.measure('direct');
+
+      client
+        .request(
+          `{
+        shop {
+          name
+        }
+      }
+      `,
+        )
+        .then(({data, errors, extensions}) => {
+          const result = performance.measure('direct');
+          setDirect(result);
+        });
+      requestMade.current = true;
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Performance metrics:</h1>
+      <h2>Oxygen timing</h2>
+      <p>{oxygen.duration}</p>
+      <h2>SFAPI Direct timing</h2>
+      <p>{direct.duration}</p>
+    </div>
+  );
+}
