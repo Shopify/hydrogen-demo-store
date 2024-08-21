b1d4750e943ca0faefbfb1439ff86bf8d35d35ad
Bret Little <bret.little@shopify.com>
2024-08-21 10:22:43 -0400

    Fix sitemap xml links

--- app/lib/sitemap.ts
+++ app/lib/sitemap.ts
@@ -9,7 +9,7 @@ const SITEMAP_INDEX_PREFIX = `<?xml version="1.0" encoding="UTF-8"?>
 const SITEMAP_INDEX_SUFFIX = `</sitemapindex>`;
 
 const SITEMAP_PREFIX = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">`;
 const SITEMAP_SUFFIX = `</urlset>`;
 
 type Locale = `${LanguageCode}-${CountryCode}`;
