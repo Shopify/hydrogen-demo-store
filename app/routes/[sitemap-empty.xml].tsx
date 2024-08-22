dacfe3c9c2acd10e11611de8ddcde81e5a396696
Bret Little <bret.little@shopify.com>
2024-08-22 15:53:30 -0400

    Add a link to the empty sitemap

--- app/routes/[sitemap-empty.xml].tsx
+++ app/routes/[sitemap-empty.xml].tsx
@@ -1,6 +1,12 @@
-export async function loader() {
+import type {LoaderFunctionArgs} from '@remix-run/server-runtime';
+
+export async function loader({request}: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const baseUrl = url.origin;
+
   return new Response(
     `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+  <url><loc>${baseUrl}/</loc></url>
 </urlset>`,
     {
       headers: {
