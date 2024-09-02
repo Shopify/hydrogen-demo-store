2c6a6a0c7d22b37438b1ba5f0441d53d56c7cc91
Stefan <stefan@balbis.studio>
2024-09-02 18:37:40 +0100

    update default locale

--- server.ts
+++ server.ts
@@ -46,7 +46,7 @@ export default {
       const {storefront} = createStorefrontClient({
         cache,
         waitUntil,
-        i18n: getLocaleFromRequest(request),
+        i18n: {language: 'EN', country: 'GB'},
         publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
         storeDomain: env.PUBLIC_STORE_DOMAIN,
