386d146a6fd8e592d772204f6a01d46e11179a02
Helen Lin <helen.lin@shopify.com>
2024-10-30 16:55:31 -0700

    fix test and more updates

--- env.d.ts
+++ env.d.ts
@@ -28,6 +28,7 @@ declare global {
     PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID: string;
     PUBLIC_CUSTOMER_ACCOUNT_API_URL: string;
     PUBLIC_CHECKOUT_DOMAIN: string;
+    SHOP_ID: string;
   }
 }
 
--- playwright.config.ts
+++ playwright.config.ts
@@ -41,6 +41,8 @@ let config: PlaywrightTestConfig = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    bypassCSP: true,
   },
 
   /* Configure projects for major browsers */
