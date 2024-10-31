ef9120bca07991d5d3d3af055d48c6737ff83a1d
Juan P. Prieto <jp@calltheguys.co>
2024-10-31 10:48:23 -0700

    Add hydrogen sitemap routes

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -1,23 +1,17 @@
 import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
 
-import {getSitemapIndex} from 'app/lib/sitemap';
+import {getSitemapIndex} from '@shopify/hydrogen';
 
 export async function loader({
   request,
   context: {storefront},
 }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
-  const baseUrl = url.origin;
-
   const response = await getSitemapIndex({
     storefront,
     request,
-    types: ['products', 'pages', 'collections', 'articles'],
-    customUrls: [`${baseUrl}/sitemap-empty.xml`],
   });
 
-  response.headers.set('Oxygen-Cache-Control', `max-age=${60 * 60 * 24}`);
-  response.headers.set('Vary', 'Accept-Encoding, Accept-Language');
+  response.headers.set('Cache-Control', `max-age=${60 * 60 * 24}`);
 
   return response;
 }
